b66c399bb4877899c807c22d4116935ffe9566ee
Liran Tal <liran.tal@gmail.com>
2016-10-21 19:02:46 +0300

    adding functional support to toggle select specific packages and provide the install instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,32 @@ var bundles = require('./bundles');
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app works!';
-  bundlesList = bundles;
+  bundlesList: Object = bundles;
+  userBundles: Object = {};
+
+  toggleSelected(event: any, bundleName: string, packageName: string) {
+
+    if (!this.userBundles.hasOwnProperty(bundleName)) {
+      Object.defineProperty(this.userBundles, bundleName, {
+        value: []
+      });
+    }
+
+    let pkgIndex = this.userBundles[bundleName].indexOf(packageName);
+    if (pkgIndex === -1) {
+        this.userBundles[bundleName].push(packageName);
+        event.target.className = "btn btn-primary";
+    } else {
+        this.userBundles[bundleName].splice(pkgIndex, 1);
+        event.target.className = "btn btn-secondary";
+    }
+
+  }
+
+  getSelected(bundleName: string) {
+    if (this.userBundles[bundleName] && this.userBundles[bundleName].length > 0) {
+      return this.userBundles[bundleName].join(' ');
+    }
+  }
+
 }
