b97a7df579fcd6b8da571d134b02f169d01256fc
Idan Cohen <idan@idancohen.com>
2016-10-22 19:59:41 +0300

    Implementing UI structure

--- src/app/bundles.js
+++ src/app/bundles.js
@@ -3,9 +3,33 @@ module.exports = {
     {
       "name": "javascript",
       "prettyName": "JavaScript",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus sed augue a posuere. Sed elementum ultricies cursus. Donec eget suscipit metus.",
       "packages": [
         { "name": "typescript" },
-        { "name": "markdown-to-pdf" }
+        { "name": "language-babel" },
+        { "name": "linter" },
+        { "name": "markdown-to-pdf" },
+        { "name": "atom-beautify" }
+      ]
+    },
+    {
+      "name": "python",
+      "prettyName": "Python",
+      "description": "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur non auctor augue, in tempus nulla.",
+      "packages": [
+        { "name": "file-icons" },
+        { "name": "pigments" },
+        { "name": "git-time-machine" }
+      ]
+    },
+    {
+      "name": "java",
+      "prettyName": "Java",
+      "description": "Mauris a ante orci. Morbi sed nulla ullamcorper, tincidunt odio sit amet, porttitor felis. Nunc urna nisi, imperdiet eget lorem eget, bibendum dictum tellus.",
+      "packages": [
+        { "name": "minimap" },
+        { "name": "qolor" },
+        { "name": "relative-numbers" },
       ]
     }
   ]
