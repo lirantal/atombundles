7f789087e1344bbc60dcaf248c4db0ceb8ace8df
Idan Cohen <idan@idancohen.com>
2016-10-22 15:18:44 +0300

    change to sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ var bundles = require('./bundles');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
